ea02fcda357f11a30a0d8ade65cfcdcb1b51d53c
Jim Gough <jpgough@gmail.com>
2019-10-09 19:26:47 +0100

    Initial Commit

---
+++ src/main/java/com/jpgough/attendees/UserDetail.java
@@ -0,0 +1,36 @@
+package com.jpgough.attendees;
+
+public class UserDetail {
+    private Integer userId;
+    private String name;
+    private String email;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + name + "," + email + "," + userId + "}";
+    }
+}
