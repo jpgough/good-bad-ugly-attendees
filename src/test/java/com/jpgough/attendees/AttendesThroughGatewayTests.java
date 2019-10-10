d9b20a25964ee722218b47d7edbff7dea6187985
MattyA <mat.auburn@gmail.com>
2019-10-10 22:22:17 +0100

    Working SCG with local attendees service

--- src/test/java/com/jpgough/attendees/AttendesThroughGatewayTests.java
+++ src/test/java/com/jpgough/attendees/AttendesThroughGatewayTests.java
@@ -1,13 +1,11 @@
 package com.jpgough.attendees;
 
 
-import com.github.dockerjava.api.model.Volumes;
-import com.github.dockerjava.api.model.VolumesFrom;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,16 +13,20 @@ import org.springframework.web.client.RestTemplate;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+        properties = {"server.port=12345"}
+)
 @DirtiesContext
 public class AttendesThroughGatewayTests {
 
     private static final int EXPOSED_GATEWAY_PORT = 8080;
-//    private static final VolumesFroms VOLUMES_FROMS = new VolumesFrom();
 
     @Rule
-    public GenericContainer redis = new GenericContainer<>("jpgough/vanilla-scg")
+    public GenericContainer scgVanillaContainer = new GenericContainer<>("jpgough/vanilla-scg")
             .withExposedPorts(EXPOSED_GATEWAY_PORT)
             .withClasspathResourceMapping("gateway/application.yaml",
                     "/etc/gateway/config/application.yaml",
@@ -36,13 +38,13 @@ public class AttendesThroughGatewayTests {
 
     @Test
     public void hit_service(){
-        String address = redis.getContainerIpAddress();
-        Integer port = redis.getFirstMappedPort();
+        String address = scgVanillaContainer.getContainerIpAddress();
+        Integer port = scgVanillaContainer.getFirstMappedPort();
 
-        String  endpoint = "http://"+address + ":" + port + "/num/42";
+        String  endpoint = "http://"+address + ":" + port + "/a/cached?number=1";
         RestTemplate restTemplate = new RestTemplate();
-        final ResponseEntity<String> forEntity = restTemplate.getForEntity(endpoint, String.class);
-        System.out.println(forEntity);
+        final ResponseEntity<String> response = restTemplate.getForEntity(endpoint, String.class);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
 
