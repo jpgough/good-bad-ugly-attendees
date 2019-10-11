a29a24b518ee0cb9ac6b5d91771de23a521c48f8
MattyA <mat.auburn@gmail.com>
2019-10-11 13:10:23 +0100

    Adding IP resolution for the docker test

--- src/test/java/com/jpgough/attendees/AttendesThroughGatewayTests.java
+++ src/test/java/com/jpgough/attendees/AttendesThroughGatewayTests.java
@@ -13,6 +13,14 @@ import org.springframework.web.client.RestTemplate;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
@@ -27,21 +35,24 @@ public class AttendesThroughGatewayTests {
 
     @Rule
     public GenericContainer scgVanillaContainer = new GenericContainer<>("jpgough/vanilla-scg")
+
             .withExposedPorts(EXPOSED_GATEWAY_PORT)
+            .withEnv("HOST_MACHINE_IP_ADDRESS", IPUtils.myIPAddress())
             .withClasspathResourceMapping("gateway/application.yaml",
                     "/etc/gateway/config/application.yaml",
                     BindMode.READ_ONLY);
+
 
     @Test
     public void contextLoads() {
     }
 
     @Test
-    public void hit_service(){
+    public void hit_service() {
         String address = scgVanillaContainer.getContainerIpAddress();
         Integer port = scgVanillaContainer.getFirstMappedPort();
 
-        String  endpoint = "http://"+address + ":" + port + "/a/cached?number=1";
+        String endpoint = "http://" + address + ":" + port + "/a/cached?number=1";
         RestTemplate restTemplate = new RestTemplate();
         final ResponseEntity<String> response = restTemplate.getForEntity(endpoint, String.class);
         assertEquals(HttpStatus.OK, response.getStatusCode());
---
+++ src/test/java/com/jpgough/attendees/IPUtils.java
@@ -0,0 +1,41 @@
+package com.jpgough.attendees;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+public class IPUtils {
+
+    public static String myIPAddress() {
+        String candidateIPAddress = null;
+        try {
+            candidateIPAddress = InetAddress.getLocalHost().getHostAddress();
+            if (!candidateIPAddress.equals("127.0.0.1")) {
+                return candidateIPAddress;
+            }
+            final Enumeration<NetworkInterface> interfaces =
+                    NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                final NetworkInterface cur = interfaces.nextElement();
+                if (cur.isLoopback()) {
+                    continue;
+                }
+                for (final InterfaceAddress addr : cur.getInterfaceAddresses()) {
+                    final InetAddress inet_addr = addr.getAddress();
+                    if (!(inet_addr instanceof Inet4Address)) {
+                        continue;
+                    }
+                    if (addr.getBroadcast() != null) {
+                        return inet_addr.getHostAddress();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        throw new RuntimeException("Unable to find a candidate IP Address");
+    }
+}
